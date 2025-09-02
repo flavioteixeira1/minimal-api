24079fe8d205ad78cb2d1b4c3c83539c51870fe9
flavioteixeira1 <flavioteixeira1@gmail.com>
2025-09-02 09:16:39 -0300

    refatoração program.cs e Startup.cs

---
+++ Test/Helpers/Setup.cs
@@ -0,0 +1,45 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Infraestrutura.Db;
+using Microsoft.AspNetCore.Mvc.Testing;
+using MinimalApi;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.AspNetCore.Hosting;
+using MinimalApi.Dominio.Interfaces;
+using Test.Mocks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Test.Helpers;
+
+public class Setup
+{
+    public const string PORT = "5087";
+    public static TestContext testContext = default!;
+    public static WebApplicationFactory<Startup> http = default!;
+    public static HttpClient client = default!;
+
+    public static void ClassInit(TestContext testContext)
+    {
+        Setup.testContext = testContext;
+        Setup.http = new WebApplicationFactory<Startup>();
+
+        Setup.http = Setup.http.WithWebHostBuilder(builder =>
+       {
+         builder.UseSetting("https_port", Setup.PORT).UseEnvironment("Testing");
+
+         builder.ConfigureServices(services =>
+    {
+        services.AddScoped<IAdministradorServico, AdministradorServicoMock>();
+
+    });
+    });
+
+        Setup.client = Setup.http.CreateClient();
+    } 
+
+    public static void ClassCleanup() {
+        Setup.http.Dispose();
+    }
+    }
---
+++ Test/Requests/AdministradorRequestTest.cs
@@ -0,0 +1,28 @@
+using System;
+using MinimalApi.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Infraestrutura.Db;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using MinimalApi.Dominio.Servicos;
+
+namespace Test.Requests;
+
+[TestClass]
+public class AdministradorRequestTest
+{
+    [TestMethod]
+    public void Metodo()
+    {
+        //Arrange
+
+
+        //act
+
+
+
+
+        //Assert
+    }
+
+}
